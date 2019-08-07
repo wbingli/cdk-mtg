646f9c6cc4cd8b3c56c1618f0536c1edbb18cb7b
Wenbing Li <libruce@amazon.com>
2019-08-07 16:52:46 -0700

    Init import

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -2,5 +2,6 @@
 import cdk = require('@aws-cdk/core');
 import { CdkStack } from '../lib/cdk-stack';
 
+const env = {region: "us-east-1", account: "576483741303"}
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new CdkStack(app, 'CdkStack', {env: env});
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,18 +1,45 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import ecsPatterns = require('@aws-cdk/aws-ecs-patterns');
+import ecs = require('@aws-cdk/aws-ecs');
+import elbv2 = require('@aws-cdk/aws-elasticloadbalancingv2');
+import ec2 = require('@aws-cdk/aws-ec2');
+
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps){
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+   
+    const vpc =  ec2.Vpc.fromLookup(this, "VPC", {
+      tags: {"Name": "VpcStack/VPC"}
+    });
+    
+    const cluster  = new ecs.Cluster(this, "Cluster", {
+      vpc: vpc
     });
 
-    const topic = new sns.Topic(this, 'CdkTopic');
+    const loadBalancedFargateService = new ecsPatterns.LoadBalancedFargateService(this, 'Service', {
+      cluster,
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      publicLoadBalancer: true,
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const internal_lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: false
+    });
+
+    const listener = internal_lb.addListener('Listener', {
+      port: 80,
+      open: true,
+    });
+
+    listener.addTargets("ECSTG", {
+      port: 80,
+      targets: [loadBalancedFargateService.service]
+    });
+
+    new cdk.CfnOutput(this, "InternalLD", {value: internal_lb.loadBalancerDnsName})
   }
 }
